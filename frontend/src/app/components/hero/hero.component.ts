297caa06ea26d4f160fdf88cadda79048007cd4b
rosscalpe <ross.gusteban@gmail.com>
2025-08-12 22:34:16 -0300

    Modificación del hero en responsive

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,10 +1,11 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -16,4 +16,8 @@ export class NavbarComponent {
     this.menuOpen = !this.menuOpen;
   }
 
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
 }
